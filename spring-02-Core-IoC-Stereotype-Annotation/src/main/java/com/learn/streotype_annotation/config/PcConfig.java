05d6ea031eb3f30bfd613fdf213826b5e96db7a9
Sasha <alexandra.aslobodian@gmail.com>
2023-03-12 14:59:21 -0400

    created PC class and PC Config

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/learn/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,10 @@
+package com.learn.streotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.learn.streotype_annotation")
+public class PcConfig {
+
+}
