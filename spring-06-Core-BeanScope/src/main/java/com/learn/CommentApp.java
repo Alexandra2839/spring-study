23f61b3157565d3df1e55bf41f3aa54ba642ed9b
Sasha <alexandra.aslobodian@gmail.com>
2023-03-13 16:13:22 -0400

    created comment app to test singleton and prototype bean scopes

---
+++ spring-06-Core-BeanScope/src/main/java/com/learn/CommentApp.java
@@ -0,0 +1,43 @@
+package com.learn;
+
+import com.learn.config.CommentConfig;
+import com.learn.model.Comment;
+import com.learn.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+    public static void main(String[] args) {
+
+        Comment comment  = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+        System.out.println(cs1==cs2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
