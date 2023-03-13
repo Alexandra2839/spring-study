977419d55ec79eff409c527354ae81009a06b6c8
Sasha <alexandra.aslobodian@gmail.com>
2023-03-13 16:06:12 -0400

    created email and push notification proxy

---
+++ spring-05-Core-Abstraction/src/main/java/com/learn/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,15 @@
+package com.learn.proxy;
+
+import com.learn.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("EMAIL")
+public class EmailCommentNotificationProxy implements CommentNotificationProxy {
+
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending notification for comment:" + comment.getText());
+    }
+}
