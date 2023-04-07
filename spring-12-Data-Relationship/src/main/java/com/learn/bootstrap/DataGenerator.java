0d1bd48c2305586ec0c027dc1115a5f295ab1797
Sasha <alexandra.aslobodian@gmail.com>
2023-04-07 19:21:15 -0400

    added Data

--- spring-12-Data-Relationship/src/main/java/com/learn/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/learn/bootstrap/DataGenerator.java
@@ -1,22 +1,33 @@
 package com.learn.bootstrap;
 
-import com.learn.entity.Payment;
-import com.learn.entity.PaymentDetail;
+import com.learn.entity.*;
 import com.learn.enums.Status;
+import com.learn.repository.CartRepository;
+import com.learn.repository.ItemRepository;
+import com.learn.repository.MerchantRepository;
 import com.learn.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
+    private final CartRepository cartRepository;
+    private final ItemRepository itemRepository;
+
+
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, CartRepository cartRepository, ItemRepository itemRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
+        this.cartRepository = cartRepository;
+        this.itemRepository = itemRepository;
     }
 
 
@@ -33,12 +44,45 @@ public class DataGenerator implements CommandLineRunner {
 
         payment2.setPaymentDetail(paymentDetail2);
 
+
+
+
+        Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
+
+        payment1.setMerchant(merchant1);
+        payment2.setMerchant(merchant1);
+
+        Item item1 = new Item("Milk","M01");
+        Item item2 = new Item("Sugar","S01");
+        Item item3 = new Item("Bread","B01");
+
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
+
+        cart1.setItemList(Arrays.asList(item1,item2,item3));
+        cart2.setItemList(Arrays.asList(item1,item2));
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
+
+
+        merchantRepository.save(merchant1);
+
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
 
-        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
 
-        paymentRepository.delete(payment1);
+
+
+
+//        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
+//
+//        paymentRepository.delete(payment1);
+
 
     }
 }
