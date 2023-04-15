0612f9465bdf2a52ccbd324259279fd648ad103b
Sasha <alexandra.aslobodian@gmail.com>
2023-04-14 21:06:25 -0400

    created entities and added data

---
+++ spring-13-Data-Queries/src/main/java/com/learn/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.learn.entity;
+
+import com.learn.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "employees")
+@Data
+public class Employee extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private Integer salary;
+    @ManyToOne
+    @JoinColumn(name = "department")
+    private Department department;
+
+    @ManyToOne
+    private Region region;
+
+}
