6d4e1761c566b469792e46d6e5fad8b6aaa3b6dc
Sasha <alexandra.aslobodian@gmail.com>
2023-06-12 15:52:41 -0400

    added another @Pointcut and Advice

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.cydeo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,13 +14,39 @@ public class LoggingAspect {
 
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
-    public void myPointcut(){}
+//    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+//    public void myPointcut(){}
+//
+//    @Before("myPointcut()")
+//    public void log(){
+//        logger.info("Info log............");
+//    }
 
-    @Before("myPointcut()")
-    public void log(){
-        logger.info("Info log............");
-    }
+//    @Before("execution(* com.cydeo.controller.CourseController.*(..))") // better to create a pointcut separately
+//    public void log(){
+//        logger.info("Info log............");
+//    }
+
+//    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+//    public void courseRepositoryFindByIdPC() {}
+//
+//    @Before("courseRepositoryFindByIdPC()")
+//    public void beforeCourseRepositoryFindById(JoinPoint joinPoint){
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+
+//    @Pointcut("within(com.cydeo.controller..*)")
+//    public void anyControllerOperation() {}
+//
+//    @Pointcut("@within(org.springframework.stereotype.Service)")
+//    public void anyServiceOperation() {}
+//
+//    @Before("anyControllerOperation() || anyServiceOperation()")
+//    public void beforeControllerOrServiceAdvice(JoinPoint joinPoint){
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
 
 
 
