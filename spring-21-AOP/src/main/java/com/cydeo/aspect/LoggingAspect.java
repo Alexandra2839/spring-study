339e95bd16349e17bb2002ff0e9c4b79190030c6
Sasha <alexandra.aslobodian@gmail.com>
2023-06-12 19:36:40 -0400

    added @Pointcut with @AfterThrowing advice

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -2,10 +2,7 @@ package com.cydeo.aspect;
 
 import com.cydeo.dto.CourseDTO;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -70,18 +67,25 @@ public class LoggingAspect {
 //                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
 
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    public void afterReturningGetControllerOperation() {}
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+//    public void afterReturningGetControllerOperation() {}
 
 //    @AfterReturning(pointcut = "afterReturningGetControllerOperation()", returning = "result")
 //    public void afterReturningGetMappingOperation(JoinPoint joinPoint, Object result){
 //        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), result.toString());
 //    }
 
-    @AfterReturning(pointcut = "afterReturningGetControllerOperation()", returning = "results")
-    public void afterReturningGetMappingOperation(JoinPoint joinPoint, List<CourseDTO> results){
-        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), results.toString());
-    }
+//    @AfterReturning(pointcut = "afterReturningGetControllerOperation()", returning = "results")
+//    public void afterReturningGetMappingOperation(JoinPoint joinPoint, List<CourseDTO> results){
+//        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), results.toString());
+//    }
+
+//    @AfterThrowing(pointcut = "afterReturningGetControllerOperation()", throwing = "exception")
+//    public void afterThrowingGetMappingOperation(JoinPoint joinPoint, RuntimeException exception) {
+//        logger.error("After Throwing -> Method: {}, Exception: {}", joinPoint.getSignature().toShortString(), exception.getMessage());
+//
+//    }
+
 
 
 
