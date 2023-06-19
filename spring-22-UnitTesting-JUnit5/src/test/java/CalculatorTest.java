438380d58a6574ac2154f7bc7ca2ba22f84c334e
Sasha <alexandra.aslobodian@gmail.com>
2023-06-19 16:32:07 -0400

    practiced different Assertions methods

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -6,7 +6,51 @@ class CalculatorTest {
 
     @Test
     void add(){
+        System.out.println("Add method");
+        int actual = Calculator.add(2,3);
+        assertEquals(4, actual, "Test failed");
 
+    }
+
+    @Test
+    void testCase1(){
+        System.out.println("Test Case 1");
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void testCase2(){
+        System.out.println("Test Case 2");
+        assertTrue(Calculator.operator.equals("add"));
+    }
+
+    @Test
+    void testCase3(){
+        System.out.println("Test Case 3");
+        assertArrayEquals(new int[] {1,2,3}, new int[]{1,2,3}, "Arrays are not the same");
+    }
+
+    @Test
+    void testCase4(){
+        System.out.println("Test Case 4");
+
+        String nullString = null;
+        String notNullString = "Learn";
+
+        assertNull(nullString);
+        assertNotNull(notNullString);
+
+    }
+    @Test
+    void testCase5(){
+        System.out.println("Test Case 5");
+
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertSame(c1, c2);
+        assertNotSame(c1, c3);
     }
 
 }
